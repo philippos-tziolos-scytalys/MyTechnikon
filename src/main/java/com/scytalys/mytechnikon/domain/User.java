32f9985023dd16c577c58057412734e03d796526
Philippos Tziolos SCYTALYS <philippos.tziolos@scytalys.com>
2023-07-28 17:16:57 +0300

    added orphanRemoval

--- src/main/java/com/scytalys/mytechnikon/domain/User.java
+++ src/main/java/com/scytalys/mytechnikon/domain/User.java
@@ -56,11 +56,11 @@ public class User extends BaseModel{
     private Role role;
 
     @JsonManagedReference(value = "user-property")
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Property> properties;
 
     @JsonManagedReference(value = "user-report")
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Report> reports;
 
 
